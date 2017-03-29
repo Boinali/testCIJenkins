d7ba337d767b1255f3ba1de1e9f6c90b95b26453
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-03-29 16:38:12 +0200

    add class with main method

---
+++ src/co/generecite/ocr/TestGenericite.java
@@ -0,0 +1,15 @@
+package co.generecite.ocr;
+
+public class TestGenericite {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j = 20, i = 0;
+		  try {
+		    System.out.println(j/i);
+		  } catch (ArithmeticException e) {
+		    //System.out.println("Division par zéro !");
+		  }
+	}
+
+}
